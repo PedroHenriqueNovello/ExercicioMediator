dcf6596e912b7785487789d0091805dff13f19cc
PedroHenriqueNovello <phnporto@gmail.com>
2026-05-15 14:24:29 -0300

    feat: implementação da classe Usuario

---
+++ Usuario.cs
@@ -0,0 +1,18 @@
+namespace MediatorApp
+{
+    public abstract class Usuario
+    {
+        protected readonly IChatMediator _mediator;
+        public string Nome {get; private set;}
+
+        protected Usuario (IChatMediator mediador, string nome)
+        {
+            _mediador = mediador;
+            Nome = nome;
+        }
+
+        public abstract void Enviar(string mensagem);
+        public abstract void Receber (string mensagem);
+
+    }
+}
