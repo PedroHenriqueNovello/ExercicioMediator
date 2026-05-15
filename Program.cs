8e9c9b88b34ff17aa69908ada93a22b9cfe33011
PedroHenriqueNovello <phnporto@gmail.com>
2026-05-15 14:55:03 -0300

    feat: implementação da classe Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Channels;
+using Microsoft.VisualBasic;
+
+namespace MeaditorChatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IChatMediator salaGeral = new SalaDeChat();
+
+            var isabelle = new UsuarioChat (salaGeral, "Isabelle");
+            var pedro = new UsuarioChat (salaGeral, "Pedro");
+            var joao = new UsuarioChat (salaGeral, "João");
+
+            salaGeral.AdicionarUsuario(isabelle);
+            salaGeral.AdicionarUsuario(pedro);
+            salaGeral.AdicionarUsuario(joao);
+
+            isabelle.Enviar("Bom dia, grupo!");
+            pedro.Enviar("Bom dia, Isabelle! Tudo bem?");
+            joao.Enviar("Bom dia, pessoal! Como estão?");
+        }
+    }
+}
