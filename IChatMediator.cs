51d10a14025380154aa33f0e75a94d5a5652cbd3
PedroHenriqueNovello <phnporto@gmail.com>
2026-05-15 15:15:43 -0300

    fix: correções no código

--- IChatMediator.cs
+++ IChatMediator.cs
@@ -2,7 +2,7 @@ namespace MediatorChatApp
 {
     public interface IChatMediator
     {
-        void EnviaMensagem (string mensagem, Usuario remetente);
+        void EnviarMensagem (string mensagem, Usuario remetente);
         void AdicionarUsuario (Usuario usuario);
     }
 }
